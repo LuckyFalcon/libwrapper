96f2ac0d2163a3679cd4b3c8cddfa2c49883ee4a
Simon McCorkindale <simon@funx.jp>
2020-04-10 12:39:21 +0900

    Fix command to run "temporal view" as it wasn't being fed entropy properly

--- controllers/visualController.js
+++ controllers/visualController.js
@@ -7,7 +7,7 @@ const { exec } = require("child_process");
 // Used to output an image visualizing specified entropy thanks to SteveLib
 exports.visualizeEntropy = [
  (req, res, next) => {
-    var cmd = "mkdir public/" + req.query.gid + "; cd public/" + req.query.gid + "; find ../../services/entropy | grep " + req.query.gid + " | xargs cat | jq --raw-output .Entropy | temporal view - .";
+    var cmd = "mkdir -p public/" + req.query.gid + "/hex_in; cd public/" + req.query.gid + "; find ../../services/entropy | grep " + req.query.gid + " | xargs cat | jq --raw-output .Entropy > hex_in/entropy.hex; temporal view hex_in/ .; rm hex_in/entropy.hex; rmdir hex_in;";
     console.log("cmd: " + cmd);
     runCmd(cmd);
     res.redirect("/" + req.query.gid + "/external_scoring.html");
@@ -18,10 +18,13 @@ function runCmd(cmd) {
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
+            console.log(`stderr: ${stderr}`);
+            console.log(`stdout: ${stderr}`);
             return;
         }
         if (stderr) {
             console.log(`stderr: ${stderr}`);
+            console.log(`stdout: ${stderr}`);
             return;
         }
         console.log(`stdout: ${stdout}`);
